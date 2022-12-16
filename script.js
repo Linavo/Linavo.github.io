c7d85e6018165e2258cff095c1378acee735665b
Linavo <linaqvo@gmail.com>
2022-12-16 14:08:38 -0800

    added projects and JS to it

--- script.js
+++ script.js
@@ -24,3 +24,25 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+let popUp = document.querySelectorAll(".container1");
+
+// popUp.addEventListener("mouseover", () => (popUp.style.opacity = "1"));
+
+// popUp.addEventListener("mouseout", () => (popUp.style.opacity = "0"));
+
+popUp.forEach((project) => {
+  project.addEventListener("mouseenter", (e) => hoverIn(e.target));
+});
+
+popUp.forEach((project) => {
+  project.addEventListener("mouseleave", (e) => hoverOut(e.target));
+});
+
+function hoverIn(space) {
+  return (space.style.opacity = "1");
+}
+
+function hoverOut(outside) {
+  return (outside.style.opacity = "0");
+}
