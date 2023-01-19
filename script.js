86df856baaa93daf349ce744ff1387cf8590911b
Linavo <linaqvo@gmail.com>
2023-01-18 16:21:37 -0800

    readjusted website due to new addition to my projects

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ init();
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos < currentScrollPos) {
     document.querySelector("header").style.top = "0";
   } else {
     document.querySelector("header").style.top = "-70px";
